a402fd6a26c4837b7593ecfeb0ee3fa6152ea6a9
Meep3692 <Meep3692@gmail.com>
2017-03-03 13:08:57 -0500

    Finished U2A2

---
+++ U2/A2/Birthday/src/birthday/Main.java
@@ -0,0 +1,18 @@
+/*
+Author: Darian
+Date Modified: March 2, 2017
+Program: User inputs name and birthday. When the button is pressed, it outputs [NAME]'s birthday is on [BIRTHDAY]
+File: Main entry point, opens GUI
+*/
+package birthday;
+
+class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI.main(args);
+    }
+    
+}
