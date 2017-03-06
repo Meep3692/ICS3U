fb4620d9974b811a6ddeaef839239914c289fc6a
Meep3692 <Meep3692@gmail.com>
2017-03-06 13:48:45 -0500

    Changes class declaration to lambda statement

--- U2/A2/Birthday/src/birthday/GUI.java
+++ U2/A2/Birthday/src/birthday/GUI.java
@@ -122,10 +122,8 @@ public class GUI extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUI().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new GUI().setVisible(true);
         });
     }
 
