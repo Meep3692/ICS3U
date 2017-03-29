5bccb109d1d0be611183c2464065400377352800
Meep3692 <Meep3692@gmail.com>
2017-03-29 10:16:42 -0400

    U2A6: Fun With Loops: Fixed header

--- U2/A6/U2A6_FunWithLoops_DarianSmith_82/src/fwl/Main.java
+++ U2/A6/U2A6_FunWithLoops_DarianSmith_82/src/fwl/Main.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Author: Darian
+Date Modified: March 29, 2017
+IDE: Netbeans 8.2
+Program: Display every integer between two given values
+File: GUI and logic
  */
 package fwl;
 
