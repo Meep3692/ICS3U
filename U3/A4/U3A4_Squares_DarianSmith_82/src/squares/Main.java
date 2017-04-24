c1f105e491c39ae84a5d54ecd605a72164b45c21
Meep3692 <Meep3692@gmail.com>
2017-04-24 13:06:25 -0400

    U3A4 Squares: It's done, Jim

--- U3/A4/U3A4_Squares_DarianSmith_82/src/squares/Main.java
+++ U3/A4/U3A4_Squares_DarianSmith_82/src/squares/Main.java
@@ -7,12 +7,20 @@ File: Main entry point, everything else
  */
 package squares;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Meep3_000
  */
 public class Main {
     public static void main(String[] args){
-        
+        ArrayList squareList = new ArrayList(10);
+        for(int i = 0; i < 10; i++){
+            squareList.add(i, i*i);
+        }
+        for(int i = 0; i < squareList.size(); i++){
+            System.out.println("Number: " + i + " Square: " + squareList.get(i));
+        }
     }
 }
