35414462341c96cc14a3f20c7cd982b9047680e8
Meep3692 <Meep3692@gmail.com>
2017-06-12 14:02:14 -0400

    Astronomap: Got Slick2D working

---
+++ U4/A3/U4A3_Astronomap_DarianSmith_82/src/astronomap/AstronomapGame.java
@@ -0,0 +1,40 @@
+/*
+Author: Darian
+Date Modified: June 12, 2017
+IDE: Netbeans 8.2
+Program: Astronomap
+File: Game
+ */
+package astronomap;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author Meep3_000
+ */
+public class AstronomapGame extends BasicGame {
+
+    public AstronomapGame(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {
+        
+    }
+
+    @Override
+    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+        
+    }
+    
+}
